997ccc90f8382d4fdd219df2c44f2354c3709720
Samuel Medina <same.isro16@gmail.com>
2023-11-26 15:12:29 -0600

    Added bg color and made font and buttons bigger

--- BiblioV1.py
+++ BiblioV1.py
@@ -14,6 +14,7 @@ import re
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import PhotoImage
+from tkinter import OptionMenu
 
 
 # Import module 
@@ -194,39 +195,25 @@ class UI:
         self.window = tk.Tk()
         self.window.title("Library Management System")
         self.window.geometry("800x400")
+        self.window.configure(bg="#2d3e50")
         self.myLibrary = Library(self.window)
-        self.clicked = StringVar()
-        # self.add_image = None
-        # self.show_image = None
-        # self.cancel_image = None
-        # self.delete_image = None
-        # self.edit_image = None
-        # self.exit_image = None
-        # self.reserva_image = None
-        # self.search_image = None
-        
-    def set_background_image(self, window, image_path):
-        background_image = tk.PhotoImage(file=image_path)
-        background_label = tk.Label(window, image=background_image)
-        background_label.place(x=0, y=0, relwidth=1, relheight=1)   
+        self.clicked = tk.StringVar()
+        self.font_size = 16
+
+
+    def set_font_size(self, widget):
+        widget.configure(font=("Arial", self.font_size))
         
         
         
-    def load_images(self):
-        # Carga las imágenes usando PhotoImage
-        self.add_image = tk.PhotoImage(file="add.png")
-        self.show_image = tk.PhotoImage(file="show.png")
-        self.cancel_image = tk.PhotoImage(file="cancel.png")
-        self.delete_image = tk.PhotoImage(file="delete.png")
-        self.edit_image = tk.PhotoImage(file="edit.png")
-        self.exit_image = tk.PhotoImage(file="exit.png")
-        self.reserva_image = tk.PhotoImage(file="reserva.png")
-        self.search_image = tk.PhotoImage(file="search.png")
+
 
     def createStandardWindow(self, title, width, height):
         window = tk.Toplevel(self.window)
         window.title(title)
         window.geometry(f"{width}x{height}")
+
+
         return window
         
 
@@ -234,82 +221,61 @@ class UI:
         
         
         menu_frame = tk.Frame(self.window)
+        menu_frame = tk.Frame(self.window, bg="#2d3e50")
         menu_frame.pack()
 
-          
-
-
         
-        tk.Label(menu_frame, text="Library Management System", font=("Helvetica", 16)).grid(row=0, columnspan=2, pady=10, padx=10)
+        
+        tk.Label(menu_frame, text="Library Management System", font=("Arial", 25),bg="#2d3e50",fg="white").grid(row=0, columnspan=2, pady=10, padx=10)
 
-        tk.Button(menu_frame, text="Add Book", command=self.addBook).grid(row=1, column=0, pady=10)
-        tk.Button(menu_frame, text="Show Books", command=self.showBooks).grid(row=1, column=1, pady=10)
-        tk.Button(menu_frame, text="Search Books",  command=self.search).grid(row=2, column=0, pady=10)
-        tk.Button(menu_frame, text="Reserve Book", command=self.reserveBook).grid(row=2, column=1, pady=10)
-        tk.Button(menu_frame, text="Cancel Reservation",  command=self.cancel).grid(row=3, column=0, pady=10)
-        tk.Button(menu_frame, text="Edit Book", command=self.editBook).grid(row=3, column=1, pady=10)
-        tk.Button(menu_frame, text="Delete Book", command=self.deleteBook).grid(row=4, column=0, pady=10)
-        tk.Button(menu_frame, text="Exit", command=self.window.quit).grid(row=4, column=1, pady=10)
+    
+
+        tk.Button(menu_frame, text="Add Book",bg="#1bbb9c", command=self.addBook, font=("Arial", 16)).grid(row=1, column=0, pady=10)
+        tk.Button(menu_frame, text="Show Books",bg="#1bbb9c", command=self.showBooks, font=("Arial", 16)).grid(row=1, column=1, pady=10)
+        tk.Button(menu_frame, text="Search Books",bg="#1bbb9c",  command=self.search, font=("Arial", 16)).grid(row=2, column=0, pady=10)
+        tk.Button(menu_frame, text="Reserve Book",bg="#1bbb9c", command=self.reserveBook, font=("Arial", 16)).grid(row=2, column=1, pady=10)
+        tk.Button(menu_frame, text="Cancel Reservation",bg="#1bbb9c",  command=self.cancel, font=("Arial", 16)).grid(row=3, column=0, pady=10)
+        tk.Button(menu_frame, text="Edit Book",bg="#1bbb9c", command=self.editBook, font=("Arial", 16)).grid(row=3, column=1, pady=10)
+        tk.Button(menu_frame, text="Delete Book",bg="#1bbb9c", command=self.deleteBook, font=("Arial", 16)).grid(row=4, column=0, pady=10)
+        tk.Button(menu_frame, text="Exit",bg="#1bbb9c", command=self.window.quit, font=("Arial", 16)).grid(row=4, column=1, pady=10)
         
         menu_frame.mainloop()
         
-        
-
-
-   
-
-        # if opt == 1:
-        #     self.addBook()
-        # elif opt == 2:
-        #     self.showBooks()
-        #     os.system('pause >NULL')
-        # elif opt == 3:
-        #     self.buscar()
-        #     os.system('pause >NULL')
-        # elif opt == 4:
-        #     self.reserveBook()
-        #     os.system('pause >NULL')
-        # elif opt == 5:
-        #     self.cancel()
-        #     os.system('pause >NULL')
-        # elif opt == 6:
-        #     self.editBook()
-        #     os.system('pause >NULL')
-        # elif opt == 7:
-        #     self.deleteBook()
-        #     os.system('pause >NULL')
-
 
 
 
     def addBook(self):
         
-        add_book_window = self.createStandardWindow("Add Book", 300, 200)
-
+        add_book_window = self.createStandardWindow("Add Book", 400, 300)
+        add_book_window.configure(bg="#2d3e50")
         
 
-        title_label = tk.Label(add_book_window, text="Title:")
+        title_label = tk.Label(add_book_window, text="Title:",bg="#2d3e50",fg="white")
+        self.set_font_size(title_label)
         title_label.pack()
         title_entry = tk.Entry(add_book_window)
-        title_entry.pack()
+        title_entry.pack(pady=5)
 
-        author_label = tk.Label(add_book_window, text="Author:")
+        author_label = tk.Label(add_book_window, text="Author:",bg="#2d3e50",fg="white")
+        self.set_font_size(author_label)
         author_label.pack()
         author_entry = tk.Entry(add_book_window)
-        author_entry.pack()
+        author_entry.pack(pady=5)
 
-        genre_label = tk.Label(add_book_window, text="Genre:")
+        genre_label = tk.Label(add_book_window, text="Genre:",bg="#2d3e50",fg="white")
+        self.set_font_size(genre_label)
         genre_label.pack()
         genre_entry = tk.Entry(add_book_window)
-        genre_entry.pack()
+        genre_entry.pack(pady=5)
 
-        year_label = tk.Label(add_book_window, text="Publication Year:")
+        year_label = tk.Label(add_book_window, text="Publication Year:",bg="#2d3e50",fg="white")
+        self.set_font_size(year_label)
         year_label.pack()
         year_entry = tk.Entry(add_book_window)
-        year_entry.pack()
+        year_entry.pack(pady=5)
 
-        add_button = tk.Button(add_book_window, text="Add Book", command=lambda: self.addBookToLibrary(title_entry.get(), author_entry.get(), genre_entry.get(), year_entry.get(), add_book_window))
-        add_button.pack()
+        add_button = tk.Button(add_book_window, text="Add Book", font=("Arial", 12),bg="#1bbb9c", command=lambda: self.addBookToLibrary(title_entry.get(), author_entry.get(), genre_entry.get(), year_entry.get(), add_book_window))
+        add_button.pack(pady=10)
 
     def addBookToLibrary(self, title, author, genre, year, add_book_window):
         book = Book(title, author, genre, year)
@@ -324,9 +290,10 @@ class UI:
         add_book_window.destroy()
 
     def showBooks(self):
-        show_books_window = self.createStandardWindow("Show Books", 300, 200)
+        show_books_window = self.createStandardWindow("Show Books", 400, 300)
+        show_books_window.configure(bg="#2d3e50")
 
-        books_frame = tk.Frame(show_books_window)
+        books_frame = tk.Frame(show_books_window,bg="#2d3e50")
         books_frame.pack(fill=tk.BOTH, expand=True)
 
         canvas = tk.Canvas(books_frame)
@@ -338,14 +305,14 @@ class UI:
         canvas.configure(yscrollcommand=scrollbar.set)
 
         if not self.myLibrary.books:
-            tk.Label(book_list, text="No hay libros en la biblioteca.").pack(pady=10)
+            tk.Label(book_list, text="No hay libros en la biblioteca.",bg="#2d3e50", font=("Arial",12)).pack(pady=10)
         else:
             for book in self.myLibrary.books:
-                tk.Label(book_list, text=f"Title: {book.title}").pack()
-                tk.Label(book_list, text=f"Author: {book.author}").pack()
-                tk.Label(book_list, text=f"Genre: {book.genre}").pack()
-                tk.Label(book_list, text=f"Year: {book.year}").pack()
-                tk.Label(book_list, text=f"Available: {'Disponible' if book.available else 'Reservado'}").pack()
+                tk.Label(book_list, text=f"Title: {book.title}", font=("Arial",12)).pack()
+                tk.Label(book_list, text=f"Author: {book.author}", font=("Arial",12)).pack()
+                tk.Label(book_list, text=f"Genre: {book.genre}", font=("Arial",12)).pack()
+                tk.Label(book_list, text=f"Year: {book.year}", font=("Arial",12)).pack()
+                tk.Label(book_list, text=f"Available: {'Disponible' if book.available else 'Reservado'}", font=("Arial",12)).pack()
                 tk.Label(book_list, text="---------------------------").pack()
 
         def on_mousewheel(event):
@@ -393,79 +360,93 @@ class UI:
         canvas.pack(side="left", fill="both", expand=True)
 
     def search(self):
-# datatype of menu text 
         
-        
-
-        search_window = self.createStandardWindow("Search Books", 300, 200)
+        search_window = self.createStandardWindow("Search Books", 400, 300)
+        search_window.configure(bg="#2d3e50")
 
         options = ["title", "author", "genre", "year"]
         self.clicked.set("title")  # initial menu text
 
         drop = OptionMenu(search_window, self.clicked, *options)
-        drop.pack()
+        drop.pack(pady=5)
 
-        criterio_label = tk.Label(search_window, text="Criterio del libro:")
+        criterio_label = tk.Label(search_window, text="Criterio del libro:",bg="#2d3e50",fg="white")
+        self.set_font_size(criterio_label)
         criterio_label.pack()
         criterio_entry = tk.Entry(search_window)
-        criterio_entry.pack()
+        criterio_entry.pack(pady=5)
 
-        buscar_button = tk.Button(search_window, text="Buscar", command=lambda: self.myLibrary.searchUI(self.clicked.get(), criterio_entry.get()))
-        buscar_button.pack()
+        buscar_button = tk.Button(search_window, text="Buscar",bg="#1bbb9c", font=("Arial", 12), command=lambda: self.myLibrary.searchUI(self.clicked.get(), criterio_entry.get()))
+
+        buscar_button.pack(pady=10)
 
     def reserveBook(self):
-        reserve_window = self.createStandardWindow("Reserve Book", 300, 200)
+        reserve_window = self.createStandardWindow("Reserve Book", 400, 300)
+        reserve_window.configure(bg="#2d3e50")
 
-        title_label = tk.Label(reserve_window, text="Título del libro a reservar:")
+        title_label = tk.Label(reserve_window, text="Título del libro a reservar:",bg="#2d3e50",fg="white")
+        self.set_font_size(title_label)
         title_label.pack()
         title_entry = tk.Entry(reserve_window)
-        title_entry.pack()
+        title_entry.pack(pady=5)
 
-        reserve_button = tk.Button(reserve_window, text="Reservar", command=lambda: self.myLibrary.reserveBook(title_entry.get()))
-        reserve_button.pack()
+        reserve_button = tk.Button(reserve_window, text="Reservar",bg="#1bbb9c",font=("Arial", 12), command=lambda: self.myLibrary.reserveBook(title_entry.get()))
+        #self.set_font_size(reserve_button)
+        reserve_button.pack(pady=10)
 
     def cancel(self):
-        cancel_window = self.createStandardWindow("Cancel Reservation", 300, 200)
+        cancel_window = self.createStandardWindow("Cancel Reservation", 400, 300)
+        cancel_window.configure(bg="#2d3e50")
 
-        title_label = tk.Label(cancel_window, text="Título del libro a cancelar la reserva:")
+        title_label = tk.Label(cancel_window, text="Título del libro a cancelar la reserva:",bg="#2d3e50",fg="white")
+        self.set_font_size(title_label)
         title_label.pack()
         title_entry = tk.Entry(cancel_window)
-        title_entry.pack()
+        title_entry.pack(pady=5)
 
-        cancel_button = tk.Button(cancel_window, text="Cancelar Reserva", command=lambda: self.myLibrary.cancelReservation(title_entry.get()))
-        cancel_button.pack()
+        cancel_button = tk.Button(cancel_window, text="Cancelar Reserva",bg="#1bbb9c", font=("Arial", 12), command=lambda: self.myLibrary.cancelReservation(title_entry.get()))
+        #self.set_font_size(cancel_button)
+        cancel_button.pack(pady=10)
 
     def editBook(self):
-        edit_window = self.createStandardWindow("Edit Book", 300, 200)
+        edit_window = self.createStandardWindow("Edit Book", 400, 300)
+        edit_window.configure(bg="#2d3e50")
 
-        title_label = tk.Label(edit_window, text="Título del libro a editar:")
+        title_label = tk.Label(edit_window, text="Título del libro a editar:",bg="#2d3e50",fg="white")
+        self.set_font_size(title_label)
         title_label.pack()
         title_entry = tk.Entry(edit_window)
-        title_entry.pack()
+        title_entry.pack(pady=5)
 
-        attribute_label = tk.Label(edit_window, text="Atributo a editar:")
+        attribute_label = tk.Label(edit_window, text="Atributo a editar:",bg="#2d3e50",fg="white")
+        self.set_font_size(attribute_label)
         attribute_label.pack()
         attribute_entry = tk.Entry(edit_window)
-        attribute_entry.pack()
+        attribute_entry.pack(pady=5)
 
-        new_value_label = tk.Label(edit_window, text="Nuevo valor:")
+        new_value_label = tk.Label(edit_window, text="Nuevo valor:",bg="#2d3e50",fg="white")
+        self.set_font_size(new_value_label)
         new_value_label.pack()
         new_value_entry = tk.Entry(edit_window)
-        new_value_entry.pack()
+        new_value_entry.pack(pady=5)
 
-        edit_button = tk.Button(edit_window, text="Editar", command=lambda: self.myLibrary.editBook( attribute_entry.get(), new_value_entry.get(),title_entry.get()))
-        edit_button.pack()
+        edit_button = tk.Button(edit_window, text="Editar",bg="#1bbb9c", font=("Arial", 12), command=lambda: self.myLibrary.editBook( attribute_entry.get(), new_value_entry.get(),title_entry.get()))
+        #self.set_font_size(edit_button)
+        edit_button.pack(pady=10)
 
     def deleteBook(self):
-        delete_window = self.createStandardWindow("Delete Book", 300, 200)
+        delete_window = self.createStandardWindow("Delete Book", 400, 300)
+        delete_window.configure(bg="#2d3e50")
 
-        title_label = tk.Label(delete_window, text="Título del libro a eliminar:")
+        title_label = tk.Label(delete_window, text="Título del libro a eliminar:",bg="#2d3e50",fg="white")
+        self.set_font_size(title_label)
         title_label.pack()
         title_entry = tk.Entry(delete_window)
-        title_entry.pack()
+        title_entry.pack(pady=5)
 
-        delete_button = tk.Button(delete_window, text="Eliminar", command=lambda: self.myLibrary.deleteBook(title_entry.get()))
-        delete_button.pack()
+        delete_button = tk.Button(delete_window, text="Eliminar",bg="#1bbb9c", font=("Arial", 12), command=lambda: self.myLibrary.deleteBook(title_entry.get()))
+        #self.set_font_size(delete_button)
+        delete_button.pack(pady=10)
 
 
     def clear_frame(self, frame):
@@ -481,15 +462,16 @@ class Login:
         self.library = library
         self.root.title("Inicio de Sesión")
         self.root.geometry("300x200")
+        self.root.configure(bg="#2d3e50")
 
-        self.background_image = tk.PhotoImage(file="add.png")  # Reemplaza con la ruta de tu imagen
+        # self.background_image = tk.PhotoImage(file="login.png") 
 
-        # Crear un label con la imagen de fondo
-        self.background_label = tk.Label(root, image=self.background_image)
-        self.background_label.place(x=0, y=0, relwidth=1, relheight=1)  
+        # # Crear un label con la imagen de fondo
+        # self.background_label = tk.Label(root, image=self.background_image)
+        # self.background_label.place(x=0, y=0, relwidth=1, relheight=1)  
         
         # Etiqueta de nombre de usuario
-        self.username_label = tk.Label(root, text="Usuario:")
+        self.username_label = tk.Label(root, text="Usuario:", font=("Arial",12),bg="#2d3e50",fg="white")
         self.username_label.pack()
 
         # Campo de entrada de nombre de usuario
@@ -497,7 +479,7 @@ class Login:
         self.username_entry.pack()
 
         # Etiqueta de contraseña
-        self.password_label = tk.Label(root, text="Contraseña:")
+        self.password_label = tk.Label(root, text="Contraseña:", font=("Arial",12),bg="#2d3e50",fg="white")
         self.password_label.pack()
 
         # Campo de entrada de contraseña
@@ -505,8 +487,8 @@ class Login:
         self.password_entry.pack()
 
         # Botón de inicio de sesión
-        self.login_button = tk.Button(root, text="Iniciar Sesión", command=self.login)
-        self.login_button.pack()
+        self.login_button = tk.Button(root, text="Iniciar Sesión", font=("Arial",12),bg="#1bbb9c", command=self.login)
+        self.login_button.pack(pady=15)
 
     def login(self):
         # Verificar si el nombre de usuario y la contraseña son correctos
